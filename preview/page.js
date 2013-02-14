ca824ede39adcc1b8ff50f5f52fe96c8171a068c
bl-test-account <plugin.developer@bytelight.com>
2013-02-14 15:27:12 -0800

    Saved at: 2013-02-14 18:27:15 -0500

---
+++ preview/page.js
@@ -0,0 +1,12 @@
+$(function(){
+  var data = JSON.parse(BL.getContentForPreview().content[0].data);
+  
+  $('#dept').text(data.department);
+  $('#name').text(data.name);
+  
+  $('button').click(function(){
+    window.open('facetime://' + data.number)
+  });
+    
+  BL.previewReady();
+});
